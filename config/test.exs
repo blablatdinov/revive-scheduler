1eb9434a0928c767c4917789a7ee43c5b201b439
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-11-08 03:05:29 +0300

    Config pgsql for test

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,9 @@
 import Config
 
+config :revive_scheduler, ReviveScheduler.Repo,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :revive_scheduler, ReviveSchedulerWeb.Endpoint,
